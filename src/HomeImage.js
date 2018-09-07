8db7512cd8eadf02d9a52bba9805cda93e24dbb4
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-09-07 14:47:06 -0700

    Adding pages

---
+++ src/HomeImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeContent from './HomeContent';
+import HomePitch from './HomePitch';
+
+const HomeImage = props => (
+  <div>
+    <h1>HOME image stuff</h1>
+    {props.image}
+  </div>
+  );
+
+export default HomeImage;
---
+++ src/HomePitch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeContent from './HomeContent';
+import HomeImage from './HomeImage';
+
+const HomePitch = props => (
+  <div>
+    <h3>So why should you hire me?</h3>
+    <p>{props.answer}</p>
+  </div>
+  );
+
+export default HomePitch;
