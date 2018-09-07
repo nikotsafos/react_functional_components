a2199ad00407bd0fe8cd176940e154ca16f8f788
brandiw <brandimichelle.0911@gmail.com>
2018-09-07 11:53:28 -0700

    Original starting point

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import HomeContent from './HomeContent';
 
 class Home extends Component {
   componentDidMount(){
     document.title = 'Home';
   }
   render(){
-    return (<HomeContent name="YOURNAMEHERE" />);
+    return (<h1>HOME PAGE</h1>);
   }
 }
 
